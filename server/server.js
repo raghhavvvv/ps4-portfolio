d1326388e87df17839f356cc262139e610a95a51
raghhavvvv <raghavpant443@gmail.com>
2025-06-19 16:52:10 +0530

    Fix: Clean up Mongoose connection and verify Atlas config

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('✅ MongoDB Connected...'))
   .catch(err => console.error(err));
 
